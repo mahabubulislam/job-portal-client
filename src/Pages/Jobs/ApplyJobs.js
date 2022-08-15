79215f8aa1b282d4259d77af180559ae5cac4853
mahbub-webdev <mahabub2k1@gmail.com>
2022-08-15 19:59:44 +0600

    applied jobs added

--- src/Pages/Jobs/ApplyJobs.js
+++ src/Pages/Jobs/ApplyJobs.js
@@ -1,21 +1,49 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { BsFillInfoCircleFill } from 'react-icons/bs';
 import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import auth from '../../firebase.init';
 import MyProfile from '../MyProfile/MyProfile';
+import Loading from '../Shared/Loading/Loading';
 
 const ApplyJobs = () => {
-    const {details} = useParams()
-    console.log(details);
+    const [user, loading] = useAuthState(auth)
+    const { id } = useParams()
+    const { data, isLoading } = useQuery(['job'], () => axios(`http://localhost:5000/jobs/${id}`))
+  
+    if (isLoading || loading) {
+        return <Loading />
+    }
+    const { title, company } = data?.data
+    const handleApplyJob = () => {
+        if (user?.email) {
+            axios.patch(`http://localhost:5000/jobs/${company}`, {
+                user: user?.email
+            })
+                .then(res => {
+                    if (res.data.message === 'exists') {
+                       toast.error('Already Applied')
+                    }
+                    if(res.data.modifiedCount>0){
+                        toast.success('Successfully Applied')
+                    }
+                })
+
+        }
+    }
     return (
         <section>
             <section className='p-8 border rounded-md bg-blue-100 text-primary'>
-                <p className="font-medium flex mb-3"><BsFillInfoCircleFill className='mr-2 text-xl' /><span>Applying to Remote JavaScript Developer Job at UpSocial Network</span></p>
+                <p className="font-medium flex mb-3"><BsFillInfoCircleFill className='mr-2 text-xl' /><span>Applying to {title} Job at {company}</span></p>
                 <div className='flex flex-col md:flex-row items-center justify-between'>
                     <p className='w-full md:w-3/4'>Whenever you apply to an internship or fresher job, this is the resume that the employer will see. Always make sure it is up to date.</p>
-                    <button className="btn btn-primary text-white mt-5">Proceed to Application</button>
+                    <button onClick={handleApplyJob} className="btn btn-primary text-white mt-5">Proceed to Application</button>
                 </div>
             </section>
-            <MyProfile/>
+            <MyProfile />
         </section>
     );
 };
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -45,10 +45,10 @@ const Navbar = ({ children }) => {
                                 </label>
                             </div>
                             {
-                                user && <div className="dropdown dropdown-end">
+                                userInfo && <div className="dropdown dropdown-end">
                                     <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
                                         <div className="w-10 rounded-full">
-                                            <img src={user?.photoURL || "https://api.lorem.space/image/face?hash=33791"} alt='' />
+                                            <img src={user?.photoURL || "https://placeimg.com/80/80/people"} alt='' />
                                         </div>
                                     </label>
                                     <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 uppercase">
